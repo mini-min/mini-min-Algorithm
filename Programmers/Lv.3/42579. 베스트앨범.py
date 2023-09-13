dad4bf59975415cab595095cc8ee7610681896dc
MinjaeLee <2alswo7@khu.ac.kr>
2023-09-13 10:35:59 +0900

    [23.09.13] Programmers/Lv.3/베스트앨범

--- Programmers/Lv.3/42579. 베스트앨범.py
+++ Programmers/Lv.3/42579. 베스트앨범.py
@@ -1,35 +1,28 @@
-"""
-미완
 def solution(genres, plays):
+    genre_play_dict = {}
+    song_list = []
     answer = []
-    hash_dict = {}
-    total_play = {}
-    
-    # 딕셔너리에 "장르: [재생횟수, 인덱스]" 형태로 저장
-    for i in range(len(genres)):
-        if genres[i] not in hash_dict:
-            hash_dict[genres[i]] = [[plays[i], i]]
-        else:
-            hash_dict[genres[i]].append([plays[i], i])
-    
-    print(hash_dict)
-    
-    for genre in hash_dict:
-        hash_dict[genre].sort(reverse=True)
-        print(hash_dict)
-        sum = 0
-        for element in range(len(hash_dict[genre])):
-            sum += hash_dict[genre][element][0]
-        total_play[genre] = sum
 
-    #print(total_play)
-    
-    for i in sorted(total_play, reverse=True):
-        if len(hash_dict[i]) == 1:
-            answer.append(ash_dict[i][0][1])
+    # 장르별 재생횟수 합을 받는 딕셔너리 -> 많은 재생횟수 순으로 정렬
+    for i, j in zip(genres,plays):
+        if i in genre_play_dict:
+            genre_play_dict[i] += j
         else:
-            answer.append(hash_dict[i][0][1])
-            answer.append(hash_dict[i][1][1])
+            genre_play_dict[i] = j
+    sortsum = sorted(genre_play_dict.items(), key=lambda x:x[1], reverse=True)
+    
+    # song_list 생성 -> "[인덱스, 장르, 플레이]" 순으로 저장 -> 플레이 횟수를 기준으로 정렬
+    for ind, (genre, play) in enumerate(zip(genres, plays)):
+        song_list.append([ind, genre, play])
+    song_list = sorted(song_list, key=lambda x:x[2], reverse=True)
+    
+    # 인덱스를 최종 반환 리스트에 추가
+    for genre, _ in sortsum:
+        count = 0
+        for i in song_list:
+            if count == 2: break        # 카운트 2개까지만 저장
+            if i[1] == genre:
+                answer.append(i[0])     # 인덱스를 리스트에 저장
+                count += 1
     
     return answer
-"""
