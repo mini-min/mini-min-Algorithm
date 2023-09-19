2a180cb8edfcfbced71ec7af02e71f77e5ca7d3e
MinjaeLee <2alswo7@khu.ac.kr>
2023-09-19 19:50:35 +0900

    [23.09.19] Programmers/Lv.1/크기가 작은 부분 문자열/코드 수정

--- Programmers/Lv.1/147355. 크기가 작은 부분 문자열.py
+++ Programmers/Lv.1/147355. 크기가 작은 부분 문자열.py
@@ -1,34 +1,8 @@
-"""
-#1. 런타임 에러 코드
-def solution(participant, completion):
-    for pl in participant:
-        if pl in completion:
-            completion.remove(pl)
-        else:
-            return pl
-"""
-
-# 2. sorted 이용 코드 (성공)
-def solution(participant, completion):
-    participant = sorted(participant)
-    completion = sorted(completion)
+def solution(t, p):
+    answer = 0
+    part_list = [int(t[i:i+len(p)]) for i in range(len(t)-len(p)+1)]
     
-    for i, j in zip(participant, completion):
-        if i!=j: return i
-    return participant[-1]
-    
-"""
-# 3. 해시 이용 코드 (성공)
-def solution(participant, completion):
-    hashDict={}
-    sumHash=0
-    
-    for part in participant:
-        hashDict[hash(part)]=part
-        sumHash+=hash(part)
-    
-    for comp in completion:
-        sumHash-=hash(comp)
-        
-    return hashDict[sumHash]
-"""
+    for num in part_list:
+        if int(p) >= num:
+            answer += 1
+    return answer
